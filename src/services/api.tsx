c9331053488a7d44d3919928600277e7d2f271ca
carlosdoria <carlosdoria953@gmail.com>
2021-07-28 21:47:51 -0300

    refactor: update baseURL

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
-const ApiGithub = axios.create( {
+const ApiGithub = axios.create({
   baseURL: 'https://api.github.com/users/carlosdoria',
-} )
+})
 
-const ApiEmail = axios.create( {
-  baseURL: 'https://carlos-email.herokuapp.com/',
-} )
+const ApiEmail = axios.create({
+  baseURL: 'https://carlosdoria-portifolio.vercel.app/api',
+})
 
 export { ApiGithub, ApiEmail }
