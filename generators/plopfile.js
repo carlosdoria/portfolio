d84cd1ccc0e521ec7b715e1acf4cf512b3db5f19
carlosdoria <carlosdoria953@gmail.com>
2022-11-14 21:23:02 -0300

    chore: update plopfile

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -20,16 +20,6 @@ module.exports = function (plop) {
         path: '../src/components/{{pascalCase name}}/styles.ts',
         templateFile: 'templates/components/styles.ts.hbs',
       },
-      {
-        type: 'add',
-        path: '../src/components/{{pascalCase name}}/stories.tsx',
-        templateFile: 'templates/components/stories.tsx.hbs',
-      },
-      {
-        type: 'add',
-        path: '../src/components/{{pascalCase name}}/test.tsx',
-        templateFile: 'templates/components/test.tsx.hbs',
-      },
     ],
   });
 
